113df78895cbcd8c2800370a4e39339a53438de6
kiakamyabi <114905508+kiakamyabi@users.noreply.github.com>
2022-12-20 17:51:28 +0000

    Step 1

--- script.js
+++ script.js
@@ -18,17 +18,13 @@ function idRefresher(sectionCount, sectionName, buttonName){
   }
 }
 
-/*Function to remove elements. Uses the location of where the eventlistener fired and then finds the class name of what
-activated it (likely a button) and sets the found name as a variable. Repeats the process with the parent node. 
-Uses the .closest method to start its search at the eventlistener fire location (likely 'this') and climb up to the
-location of the selector then the .remove() method is called to remove*/
-//selector = Where the .remove ends its search.
-
-function deleteHandler(e, selector) {
-  let buttonClassName = e.className; //= delete-btn
-  let sectionClassName = e.parentNode.className;//= section
-  e.closest(`${selector}`).remove();
-  idRefresher(allSections, sectionClassName, buttonClassName)
+/*Function to find an element, go up its node tree and remove it from the DOM.*/
+//element = Where the .closest begins its search.
+//selector = The selector that points to where the search will conclude like a queryselector.
+//sectionName = Name of the id being inserted.
+function deleteHandler(element, selector, sectionName, buttonName) {
+  element.closest(selector).remove();
+  idRefresher(allSections, sectionName, buttonName)
 }
 
 /*Function that increments the incremented count by 1, creates a new variable that contains a template literal of
@@ -40,21 +36,21 @@ the id's of the current elements.*/
 //sectionName = The ID of the created element without the number. For the section id. For the callback.
 //buttonName = The ID of the created element without the number. For the delete button id. For the callback.
 //selector = Where the .remove ends its search in the callback.
-function createSection(className, dataName, sectionCount, sectionName, buttonName, selector) {
-  incrementCount++;
+function createSection(className, dataName, sectionCount, sectionName, buttonName) {
+  incrementCount++; 
   const newSection =
    `<div class="${className}" id="" ${dataName}="${incrementCount}">
     <h3>This is a section.<br>Long live the section.</h3>
     <p>Incremented ID: ${incrementCount}</p>
-    <button class="delete-btn" id="" onclick="deleteHandler(this, '${selector}')">Delete</button>
+    <button class="delete-btn" onclick="deleteHandler(this, '[${dataName}]', '${sectionName}')">Delete</button>
   </div>`;
   formExample.insertAdjacentHTML('beforeend', newSection);
   idRefresher(sectionCount, sectionName, buttonName);
-  console.log(selector)
 }
 
 //Adds an event listener for clicking on the button used to create a section and then runs the function.
 //e.preventDefault was needed to stop the function running on page load.
 createSectionButton.addEventListener('click', (e)=>{
   e.preventDefault()
-  createSection("section", "data-section-incremented-id", allSections, 'section', 'delete', '.section')});
+  createSection("section", "data-section-incremented-id", allSections, 'section')});
+
